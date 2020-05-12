a408a16857d1c64172b45d6ed2b49c360aca65f8
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-05-11 21:06:04 -0600

    Sandbox Changes

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -1,5 +1,6 @@
 'use strict'
 const mongoose = require('mongoose');
+//needs callback -> async/await
 
 mongoose.connect("mongodb://localhost:27017/sandbox");
 
@@ -11,34 +12,83 @@ db.on("error", function(err){
 db.once("open", function(){
     console.log("Database connection successfull")
     const Schema = mongoose.Schema;
-    const animalSchema = new Schema({
+    const AnimalSchema = new Schema({
         type:  {type: String, default: "goldfish"},
         color: {type: String, default: "golden"},
-        size:  {type: String, default: "small"},
+        size:  String,
         mass:  {type: Number, default: .005},
         name:  {type: String, default: "Fisho"}
     })
-     const Animal = mongoose.model("Animal", animalSchema);
+    AnimalSchema.statics.findSmall = function(callback){
+        //this = Animal
+        return this.find({size: "small"}, callback)
+    }
+    AnimalSchema.methods.findSameColor = function(callback){
+        //this = document
+        return this.model("Animal").find({color: this.color}, callback );
+    }
 
-     let animal = new Animal({}); //creates new Animal based on defaults
+
+    AnimalSchema.pre("save", function(next){//prior to saving, this middleware runs
+        if (this.mass >= 100){
+            this.size = "big"
+        } else if (this.mass >= 5 && this.mass < 100){
+            this.size = "medium"
+        } else {
+            this.size = "small"
+        }
+        next();
+    })
+     const Animal = mongoose.model("Animal", AnimalSchema);
+
+     const animal = new Animal({}); //creates new Animal based on defaults
 
      const elephant = new Animal({
         type: "elephant",
         color: "grey",
-        size: "large", 
         mass: 6000,
         name: "Lawrence"
      })
 
-     Animal.remove({}, function(){ //removes everything from Animal collection prior to saves starting
-        elephant.save(function(err){
+     const animalData = [
+        {
+            type: "mouse",
+            color: "grey",
+            mass: .03,
+            name: "Marvin"
+         }, {
+            type: "nutria",
+            color: "brown",
+            mass: 6.35,
+            name: "Ratty"
+         }, {
+            type: "coyote",
+            color: "brown",
+            mass: 25,
+            name: "Oglo"
+         }, 
+         elephant,
+         animal
+     ]
+
+     Animal.remove({}, async function(err){ //removes everything from Animal collection prior to saves starting
             if (err) console.error("Save Failed ", err);
-            animal.save(function(err){     //db closes after animal is finished saving
+            await Animal.create(animalData, function(err, animals){ 
+                if(err) console.error(err);
                 console.log("Save success")
-                db.close(function(){
-                    console.log("db connection closed")
+                Animal.findSmall(function(err, animals){
+                    animals.forEach(element => {
+                        console.log(element.name + " is a " + element.type);
+                    });
+                })
                 });
+            await Animal.findOne({type: "elephant"}, function(err, elephant){
+                elephant.findSameColor(function(err, animals){
+                    console.log(animals)
+            })
+                db.close(function(){ //db closes after animal is finished saving
+                    console.log("db connection closed")
             });
-        });
      }); 
+    });
 })
