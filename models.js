75dd5e62a3dc66281de01fb4e0a0b062f2e5da94
Aeron Roemer <57688724+AeronRoemer@users.noreply.github.com>
2020-05-14 12:23:50 -0600

    Q&A Models

--- models.js
+++ models.js
@@ -1,6 +1,13 @@
 'use strict'
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const sortAnswers = function(a, b){
+    //for pre-save on question Schema
+    if (a.votes === b.votes){ //JS dates are stores as milliseconds
+        return b.updatedAt -a.updatedAt;
+    }
+    return b.votes - a.votes;
+}
 
 const AnswerSchema = new Schema({
     text: String,
@@ -9,12 +16,20 @@ const AnswerSchema = new Schema({
     votes: {type: Number, default: 0}
 });
 
+AnswerSchema.methods.update = function(updates, callback){
+
+};
+
 const questionSchema = new Schema({
     text: String,
     createdAt: {type: Date, default: Date.now},
     answers: [AnswerSchema]
 });
 
+questionSchema.pre("save", function(next){
+    this.answers.sort(sortAnswers);
+    next();
+})
 const Question = mongoose.model("Question", questionSchema);
 
 module.exports.Question = Question;
